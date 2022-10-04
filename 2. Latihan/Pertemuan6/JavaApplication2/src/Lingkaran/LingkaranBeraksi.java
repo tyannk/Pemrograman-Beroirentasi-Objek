c349fa5da45ab42ada2565c1d7fa72b563f97360
tyannk <41520010057@student.mercubuana.ac.id>
2022-10-04 12:20:39 +0700

    Menambahkan file yang dikerjakan pada pertemuan 6

---
+++ 2. Latihan/Pertemuan6/JavaApplication2/src/Lingkaran/LingkaranBeraksi.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lingkaran;
+
+/**
+ *
+ * @author LABPUSAT
+ */
+public class LingkaranBeraksi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double r = 11.78;
+        Lingkaran lingkaranKu = new Lingkaran();
+        lingkaranKu.hitungLuas(r);
+    }
+    
+}
